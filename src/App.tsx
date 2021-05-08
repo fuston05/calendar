80c19f47a5f9f2a64b1b09f50c69b4202db76228
scott fuston <fuston05@yahoo.com>
2021-05-07 22:26:34 -0400

    adds useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,45 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useRef } from "react";
 import { Calendar } from "./components/Calendar";
 
 // styles
 import "./app.css";
+import { log } from "node:console";
 
 function App() {
-  
-  let calendarEle: any;
+  let calRef = useRef<HTMLDivElement>(null);
+  // let calendarEle: any;
 
-  useEffect(() => {
-    calendarEle =  document.getElementById('calCont')
-  }, [])
-  
+  // useEffect(() => {
+  //   // calendarEle= document.getElementById('calCont')
+  // }, []);
 
   const startCalendly = () => {
-    if (calendarEle.style.display === 'none') {
-      calendarEle.style.display = 'block';
-    } else {
-      calendarEle.style.display = 'none';
+    // if (calendarEle.style.display === "none") {
+    //   calendarEle.style.display = "block";
+    // } else {
+    //   calendarEle.style.display = "none";
+    // }
+    if (calRef.current) {
+      if (calRef.current.style.display = "none") {
+        calRef.current.style.display = "block";
+      } else {
+        console.log('toggle to none')
+        calRef.current.style.display = "none";
+      }
     }
-  }
+  };
 
-  return <div className="App">
-    <Calendar />
-    <button style={{ position: 'absolute', top: 0, right: 0}} onClick={startCalendly} >Schedule</button>
-  </div>;
+  return (
+    <div className="App">
+      <Calendar ref={calRef} />
+      <button
+        style={{ position: "absolute", top: 0, right: 0 }}
+        onClick={startCalendly}
+      >
+        Schedule
+      </button>
+    </div>
+  );
 }
 
 export default App;
